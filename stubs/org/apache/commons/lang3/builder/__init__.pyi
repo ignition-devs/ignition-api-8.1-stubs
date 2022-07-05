6a58752db578c10b9ab0c28096cd9a667f185001
César Román <thecesrom@gmail.com>
2022-07-05 16:58:36 -0700

    feat(ia): implement all LoggerEx functions
    This change adds the following supporting packages:
    
    * org.apache.lang3
    * org.slf4j

---
+++ stubs/org/apache/commons/lang3/builder/__init__.pyi
@@ -0,0 +1,18 @@
+from java.lang import Object, StringBuffer
+from typing import Any, Union
+
+String = Union[str, unicode]
+
+class ToStringStyle(Object):
+    DEFAULT_STYLE: ToStringStyle
+    JSON_STYLE: ToStringStyle
+    MULTI_LINE_STYLE: ToStringStyle
+    NO_CLASS_NAME_STYLE: ToStringStyle
+    NO_FIELD_NAMES_STYLE: ToStringStyle
+    SHORT_PREFIX_STYLE: ToStringStyle
+    SIMPLE_STYLE: ToStringStyle
+    def append(self, buffer: StringBuffer, fieldName: String, *args: Any) -> None: ...
+    def appendEnd(self, buffer: StringBuffer, obj: Any) -> None: ...
+    def appendStart(self, buffer: StringBuffer, obj: Any) -> None: ...
+    def appendSuper(self, buffer: StringBuffer, superToString: String) -> None: ...
+    def appendToString(self, buffer: StringBuffer, toString: String) -> None: ...
