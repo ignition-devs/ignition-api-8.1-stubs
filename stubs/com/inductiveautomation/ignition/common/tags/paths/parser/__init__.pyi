c241ed77cdd921852f6d8fda86d4094d0a3271fb
César Román <thecesrom@gmail.com>
2022-10-12 08:30:47 -0700

    feat(ia): add TagPathParser class (#58)
    and supporting classes

---
+++ stubs/com/inductiveautomation/ignition/common/tags/paths/parser/__init__.pyi
@@ -0,0 +1,24 @@
+from typing import Any, List
+
+from com.inductiveautomation.ignition.common.tags.model import TagPath as TagPath
+from java.lang import Object
+from java.lang import String as String
+
+class TagPathParser(Object):
+    PARENT_RELATIVE: String
+    PATH_SEPARATOR: String
+    PROPERTY_SEPARATOR: String
+    RELATIVE_DIR_UP: String
+    ROOT_RELATIVE: String
+    @staticmethod
+    def chopPath(string: String) -> List[String]: ...
+    @staticmethod
+    def derelativize(tagPath: TagPath, relativeRoot: TagPath) -> TagPath: ...
+    @staticmethod
+    def isRelativePath(path: TagPath) -> bool: ...
+    @staticmethod
+    def parse(*args: Any) -> TagPath: ...
+    @staticmethod
+    def parseSafe(*args: Any) -> TagPath: ...
+    @staticmethod
+    def relativize(newPath: TagPath, relativeTo: TagPath) -> TagPath: ...
